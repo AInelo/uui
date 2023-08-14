b427efd35cfa6d51bebdd8f93833a528092596f6
TOTON Lionel <totonlionel@gmail.com>
2023-08-14 17:02:50 +0100

    Le commit cool

--- extrait.cpp
+++ extrait.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <cstring>
 
+bool isPalindrome(const std::string &str)
+{
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right)
+    {
+        if (str[left] != str[right])
+        {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+
+    return true;
+}
 
 int main()
 {
@@ -4883,6 +4902,23 @@ int main()
 }
 
 int main()
+{
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (isPalindrome(input))
+    {
+        std::cout << input << " is a palindrome.";
+    }
+    else
+    {
+        std::cout << input << " is not a palindrome.";
+    }
+
+    return 0;
+}int main()
 {
     std::string input;
 
