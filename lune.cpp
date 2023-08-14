5a4946161eca02c50a626495110680601af6c300
TOTON Lionel <totonlionel@gmail.com>
2023-08-14 17:06:49 +0100

    Le commit cool

--- lune.cpp
+++ lune.cpp
@@ -1,23 +1,5 @@
-
-
-int main()
-{
-    std::string input;
-
-    std::cout << "Enter a string: ";
-    std::cin >> input;
-
-    if (isPalindrome(input))
-    {
-        std::cout << input << " is a palindrome.";
-    }
-    else
-    {
-        std::cout << input << " is not a palindrome.";
-    }
-
-    return 0;
-}
+#include <iostream>
+#include <cstring>
 
 int main()
 {
@@ -75,6 +57,25 @@ int main()
 
     return 0;
 }
+
+int main()
+{
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (isPalindrome(input))
+    {
+        std::cout << input << " is a palindrome.";
+    }
+    else
+    {
+        std::cout << input << " is not a palindrome.";
+    }
+
+    return 0;
+}
 int main()
 {
     std::string input;
@@ -113,6 +114,24 @@ int main()
     return 0;
 }
 
+int main()
+{
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (isPalindrome(input))
+    {
+        std::cout << input << " is a palindrome.";
+    }
+    else
+    {
+        std::cout << input << " is not a palindrome.";
+    }
+
+    return 0;
+}
 int main()
 {
     std::string input;
