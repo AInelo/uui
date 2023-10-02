265dba70315c24ee61b0bc8391a52d45d8682acd
TOTON Lionel <totonlionel@gmail.com>
2023-10-02 12:34:54 +0100

    Le commit cool

--- main.cpp
+++ main.cpp
@@ -1081,3 +1081,22 @@ int main()
     return 0;
 }
 
+int main()
+{
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (isPalindrome(input))
+    {
+        std::cout << input << " is a palindrome.";
+    }
+    else
+    {
+        std::cout << input << " is not a palindrome.";
+    }
+
+    return 0;
+}
+
