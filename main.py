31e5eab3a5d32a58f3da688df0f210e16da4fc1e
TOTON Lionel <totonlionel@gmail.com>
2023-10-20 11:24:04 +0100

    Le commit cool

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def fibonacci(n):
             fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
         return fib_sequence
 
-def append_code_to_file(code, filename='copy.py'):
+def append_code_to_file(code, filename='main.cpp'):
     with open(filename, 'a') as file:
         file.write(code + '\n\n')  # Ajouter une ligne vide entre chaque ajout de code
 
@@ -21,18 +21,24 @@ def execute_shell_script():
     subprocess.run(['sh', 'script.sh'])
 
 if __name__ == "__main__":
-    code = """def fibonacci(n):
-    if n <= 0:
-        return "Entrez un entier positif pour calculer la suite de Fibonacci."
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
-    else:
-        fib_sequence = [0, 1]
-        while len(fib_sequence) < n:
-            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-        return fib_sequence"""
+    code = """int main()
+{
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+
+    if (isPalindrome(input))
+    {
+        std::cout << input << " is a palindrome.\n";
+    }
+    else
+    {
+        std::cout << input << " is not a palindrome.\n";
+    }
+
+    return 0;
+}"""
 
     for iteration in range(10):
         append_code_to_file(code)
