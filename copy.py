46580ea5bbf1299f9422432ecbee0eee2986c1fa
TOTON Lionel <totonlionel@gmail.com>
2023-10-21 11:20:39 +0100

    Le commit cool

--- copy.py
+++ copy.py
@@ -414,3 +414,16 @@ def fibonacci(n):
             fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
         return fib_sequence
 
+def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
