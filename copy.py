9a2ec7ebf6da5b3c7eef75e27cfda6a11f06a399
TOTON Lionel <totonlionel@gmail.com>
2023-10-31 11:03:07 +0100

    Le commit cool

--- copy.py
+++ copy.py
@@ -1,1 +1,12 @@
-la
+def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import random
-import string
 import subprocess
 
-def generate_random_code():
-    code_length = random.randint(50, 100)
-    code = 'la'
-    return code
+def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
 
 def write_code_to_file(code, filename='copy.py'):
     with open(filename, 'w') as file:
@@ -15,11 +21,23 @@ def execute_shell_script():
     subprocess.run(['sh', 'script.sh'])
 
 if __name__ == "__main__":
+    code = """def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence"""
+
     for iteration in range(6):
-        random_code = generate_random_code()
-        write_code_to_file(random_code)
-        print(f"Code généré et écrit dans le fichier copy.py (itération {iteration + 1})")
-        
+        write_code_to_file(code)
+        print(f"Code écrit dans le fichier copy.py (itération {iteration + 1})")
+
         execute_shell_script()
         print("Commande 'sh script.sh' exécutée.")
 
