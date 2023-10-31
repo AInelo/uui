d46549384f4c1f4ad95fd5e5bf37cee7d8c8ae49
TOTON Lionel <totonlionel@gmail.com>
2023-10-31 10:58:59 +0100

    Le commit cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import string
+import subprocess
 
 def generate_random_code():
     code_length = random.randint(50, 100)
@@ -10,7 +11,16 @@ def write_code_to_file(code, filename='copy.py'):
     with open(filename, 'w') as file:
         file.write(code)
 
+def execute_shell_script():
+    subprocess.run(['sh', 'script.sh'])
+
 if __name__ == "__main__":
-    random_code = generate_random_code()
-    write_code_to_file(random_code)
-    print(f"Code généré et écrit dans le fichier copy.py")
+    for iteration in range(6):
+        random_code = generate_random_code()
+        write_code_to_file(random_code)
+        print(f"Code généré et écrit dans le fichier copy.py (itération {iteration + 1})")
+        
+        execute_shell_script()
+        print("Commande 'sh script.sh' exécutée.")
+
+    print("Toutes les itérations sont terminées.")
