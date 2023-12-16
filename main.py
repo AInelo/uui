e6997f972a0c5bdd4a4f7504cced23b16f2a7b2c
TOTON Lionel <totonlionel@gmail.com>
2023-12-16 11:10:44 +0100

    Le commit cool

---
+++ main.py
@@ -0,0 +1,44 @@
+import subprocess
+
+def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+def append_code_to_file(code, filename='copy.py'):
+    with open(filename, 'a') as file:
+        file.write(code + '\n\n')  # Ajouter une ligne vide entre chaque ajout de code
+
+def execute_shell_script():
+    subprocess.run(['sh', 'script.sh'])
+
+if __name__ == "__main__":
+    code = """def fibonacci(n):
+    if n <= 0:
+        return "Entrez un entier positif pour calculer la suite de Fibonacci."
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence"""
+
+    for iteration in range(6):
+        append_code_to_file(code)
+        print(f"Code ajouté dans le fichier copy.py (itération {iteration + 1})")
+
+        execute_shell_script()
+        print("Commande 'sh script.sh' exécutée.")
+
+    print("Toutes les itérations sont terminées.")
