51596528356c9c7092bcc6b2856af3106529bb40
TOTON Lionel <totonlionel@gmail.com>
2024-01-08 22:52:39 +0100

    Le commit cool

---
+++ java.js
@@ -0,0 +1,12 @@
+for (jour = 1; jour <= 7; jour++) {
+    if (semaine[jour] = weekend) {
+        ReposeToi();
+    }
+    else {
+        TravailDur(intelligement);
+    }
+}
+
+--------------------------------------------------
+
+    a) True                           b) False 
--- lune.cpp
+++ lune.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 
+
 int main()
 {
     std::string input;
